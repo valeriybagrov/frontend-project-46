92b6db6cdef52c8e513c6376a59e8bcec678088d
valeriybagrov <valbagrov@yandex.ru>
2024-05-28 19:54:30 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -19,53 +19,86 @@ const gendiff = (path1, path2) => {
   const iter = (node1, node2, depth) => {
     const spaceCount = 4 * depth - 2;
 
-    const keys1 = _.orderBy(Object.keys(node1));
-    const keys2 = _.orderBy(Object.keys(node2));
+    const keys1 = _.sortBy(Object.keys(node1));
+    const keys2 = _.sortBy(Object.keys(node2));
 
-    const firstPart = keys1.flatMap((key) => {
+    const commonKeys = _.sortBy(_.union(keys1, keys2));
+
+    const final = commonKeys.map((key) => {
       const objToSend = new Object();
       objToSend.spaces = spaceCount;
       objToSend.key = key;
-      const value1 = node1[key];
-  
-      if (keys2.includes(key)) {
+
+      if (keys1.includes(key) && keys2.includes(key)) {
+        const value1 = node1[key];
         const value2 = node2[key];
-      
-        if (!_.isObject(value1) && !_.isObject(value2)) {
-          if (value1 === value2) {
-            objToSend.status = 'same';
-            objToSend.oldValue = value1;
-            objToSend.newValue = value1;
-          } else {
-            objToSend.status = 'updated';
-            objToSend.oldValue = value1;
-            objToSend.newValue = value2;
-          }
-        } else return `${' '.repeat(spaceCount)}  ${key}: ${iter(value1, value2, depth + 1)}`;
-      } else {
+
+        if (_.isObject(value1) && _.isObject(value2)) {
+          return `${' '.repeat(spaceCount + 2)}${key}: ${iter(value1, value2, depth + 1)}`;
+        } else {
+        objToSend.status = 'updated';
+        objToSend.value = value1;
+        objToSend.newValue = value2;
+      }
+      }
+      else if (keys1.includes(key) && !keys2.includes(key)) {
         objToSend.status = "rejected";
-        objToSend.oldValue = value1;
-        objToSend.newValue = value1;
+        objToSend.value = node1[key];
+      } else {
+        objToSend.status = 'added';
+        objToSend.value = node2[key];
       }
       return toFormStr(objToSend);
     });
 
-    const secondPart = keys2
-    .filter((key) => !keys1.includes(key))
-    .flatMap((key) => {
-      const objToSend = new Object();
+    return ['{', ...final, `${' '.repeat(spaceCount - 2)}}`].join('\n');
+    // const firstPart = keys1.flatMap((key) => {
+    //   const objToSend = new Object();
+    //   objToSend.spaces = spaceCount;
+    //   objToSend.key = key;
+    //   const value1 = node1[key];
+  
+    //   if (keys2.includes(key)) {
+    //     const value2 = node2[key];
+      
+    //     if (!_.isObject(value1) && !_.isObject(value2)) {
+    //       if (value1 === value2) {
+    //         objToSend.status = 'same';
+    //         objToSend.oldValue = value1;
+    //         objToSend.newValue = value1;
+    //       } else {
+    //         objToSend.status = 'updated';
+    //         objToSend.oldValue = value1;
+    //         objToSend.newValue = value2;
+    //       }
+    //     } else return `${' '.repeat(spaceCount)}  ${key}: ${iter(value1, value2, depth + 1)}`;
+    //   } else {
+    //     objToSend.status = "rejected";
+    //     objToSend.oldValue = value1;
+    //     objToSend.newValue = value1;
+    //   }
+    //   return toFormStr(objToSend);
+    // });
+
+    // const secondPart = keys2
+    // .filter((key) => !keys1.includes(key))
+    // .flatMap((key) => {
+    //   const objToSend = new Object();
     
-      objToSend.status = 'added';
-      objToSend.key = key;
-      objToSend.oldValue = node2[key];
-      objToSend.newValue = node2[key];
-      objToSend.spaces = spaceCount;
-      return toFormStr(objToSend);
-    });
-    const full = firstPart.concat(secondPart);
-    return ['{', ...full, `${' '.repeat(spaceCount - 2)}}`].join('\n');
+    //   objToSend.status = 'added';
+    //   objToSend.key = key;
+    //   objToSend.oldValue = node2[key];
+    //   objToSend.newValue = node2[key];
+    //   objToSend.spaces = spaceCount;
+    //   return toFormStr(objToSend);
+    // });
+    // const full = firstPart.concat(secondPart);
+    // return ['{', ...full, `${' '.repeat(spaceCount - 2)}}`].join('\n');
   };
   return iter(data1, data2, 1);
 };
 
 export default gendiff;
+no-irregular-whitespace
+no-multi-spaces
+camelcase     
--- src/stylish.js
+++ src/stylish.js
@@ -1,12 +1,12 @@
 const formater = (obj) => {
-  const { status, key, oldValue, newValue, spaces } = obj;
+  const { status, key, value, newValue, spaces } = obj;
   let sign = '';
-  if (status === 'updated') return `${' '.repeat(spaces)}- ${key}: ${oldValue}
+  if (status === 'updated') return `${' '.repeat(spaces)}- ${key}: ${value}
 ${' '.repeat(spaces)}+ ${key}: ${newValue}`;
   else if (status === 'same') sign = ' ';
   else if (status === 'rejected') sign = '-';
   else if (status === 'added') sign = '+';
-  return `${' '.repeat(spaces)}${sign} ${key}: ${typeof(oldValue) === 'object' ? JSON.stringify(oldValue, ' ', 5) : oldValue
+  return `${' '.repeat(spaces)}${sign} ${key}: ${typeof(value) === 'object' ? JSON.stringify(value, ' ', 5) : value
   }`;
 };
 

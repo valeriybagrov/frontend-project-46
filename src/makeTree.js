48010c8f543d4e3bb188910f84434e2b180c1d33
valeriybagrov <valbagrov@yandex.ru>
2024-06-07 12:49:17 +0300

    divided makeTree func into 3 func

--- src/makeTree.js
+++ src/makeTree.js
@@ -1,37 +1,58 @@
 import _ from "lodash";
 
+const checkKeyStatus = (key, keyCollection1, keyCollection2, value1, value2) => {
+  let status;
+
+  if (keyCollection1.includes(key) && keyCollection2.includes(key)) { 
+
+    if (_.isObject(value1) && _.isObject(value2)) status = 'nested';
+    else if (value1 === value2) status = 'unchanged';
+    else status = 'updated';
+
+  } else if (keyCollection1.includes(key) && !keyCollection2.includes(key)) status = 'rejected';
+  else status = 'added';
+
+  return status;
+};
+
+const getNodeValues = (status, value1, value2) => {
+  let value;
+  let newValue = undefined;
+  
+  switch (status) {
+    case 'nested':
+      value = makeTree(value1, value2);
+      break;
+    case 'unchanged':
+      value = value1;
+      break;
+    case 'updated':
+      value = value1;
+      newValue = value2;
+      break;
+    case 'rejected':
+      value = value1;
+      break;
+    case 'added':
+      value = value2;
+      break;
+    default:
+      break;
+  }
+  return { value, newValue };
+};
+
 const makeTree = (node1, node2) => {
   const keys1 = _.sortBy(Object.keys(node1));
   const keys2 = _.sortBy(Object.keys(node2));
   const commonKeys = _.sortBy(_.union(keys1, keys2));
 
   const tree = commonKeys.map((key) => {
-    let status;
-    let value;
-    let newValue = undefined;
-
     const value1 = node1[key];
     const value2 = node2[key];
+    const status = checkKeyStatus(key, keys1, keys2, value1, value2);
 
-    if (keys1.includes(key) && keys2.includes(key)) {  
-      if (_.isObject(value1) && _.isObject(value2)) {
-        status = 'nested';
-        value = makeTree(value1, value2);
-      } else if (value1 === value2) {
-        status = 'unchanged';
-        value = value1;
-      } else {
-        status = 'updated';
-        value = value1;
-        newValue = value2;
-      }
-    } else if (keys1.includes(key) && !keys2.includes(key)) {
-      status = 'rejected';
-      value = value1;
-    } else {
-      status = 'added';
-      value = value2;
-    }
+    const { value, newValue } = getNodeValues(status, value1, value2);
 
     return { status, key, value, newValue };
   });
